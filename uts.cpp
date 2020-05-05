98b0085746f7f1599ba6b48c4fffc3cfcc01533d
r-santoso <58503534+r-santoso@users.noreply.github.com>
2020-05-06 04:33:09 +0700

    Create uts.cpp

---
+++ uts.cpp
@@ -0,0 +1,56 @@
+//program hitung gaji karyawan
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+ int main() {
+    char nama[50];
+    double nim, gajiPokok, tunjanganAnak, tunjanganIstri, gajiTotal, i, j, nim;
+    int jumlahAnak;
+    
+    cout<<"Input Nim : ";
+    cin>>nim;
+    cout<<"Input Nama : ";
+    cin>>nama;
+    cout<<"Input jumlah anak : ";
+    cin>>jumlahAnak;
+    
+    //hitung gaji pokok
+    gajiPokok = nim / 1000;
+    
+    //hitung tunjangan istri
+    if(jumlahAnak != 0){
+    	tunjanganIstri = gajiPokok * 10 / 100;
+	} else { 
+		tunjanganIstri = gajiPokok * 5 / 100;
+	}
+	
+	//hitung tunjangan anak
+	if(jumlahAnak <= 3){
+		i = jumlahAnak;
+		j = i * 5 / 100;
+		tunjanganAnak = j * gajiPokok;
+	} else if(jumlahAnak > 3){
+		tunjanganAnak = (gajiPokok * 3) * 5 / 100;
+	}
+	
+	//hitung gaji total
+	gajiTotal = gajiPokok + tunjanganIstri + tunjanganAnak;
+	
+	//cetak hasil
+	cout<<"\n\t********* PROGRAM MENGHITUNG GAJI KARYAWAN *********\n";
+	cout<<"\tNIM\t\t: "<<nim;
+	cout<<"\n\tNama\t\t: "<<nama;
+	cout<<"\n\tStatus\t\t: Menikah";
+	cout<<"\n\t----------------------------------------------------";
+	cout<<"\n\tJumlah Anak\t\t: "<<jumlahAnak;
+	cout<<"\n\n\tGaji Pokok\t\t: "<<setiosflags(ios::fixed)<<setprecision(0)<<gajiPokok;
+	cout<<"\n\n\tTunjangan Anak\t\t: "<<setiosflags(ios::fixed)<<setprecision(0)<<tunjanganAnak;
+	cout<<"\n\n\tTunjangan Istri\t\t: "<<setiosflags(ios::fixed)<<setprecision(0)<<tunjanganIstri;
+	cout<<"\n\n\tTotal Gaji\t\t: "<<setiosflags(ios::fixed)<<setprecision(0)<<gajiTotal;
+	cout<<"\n\t****************************************************\n";
+	
+  return 0;
+}
